5136faeced76cde141abab312dacbb175c1500ac
annairahomespa <annaira.homespa@gmail.com>
2026-01-08 14:07:21 +0800

    added landing page

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import streamlit as st
 st.set_page_config(page_title="Annaira Home Spa", layout="wide")
 
 # Inisialisasi Halaman
-home_page = st.Page("views/home.py", title="Home", icon="🏠", default=True)
+home_page = st.Page("views/home.py", title="Home", icon="🏠")
 admin_page = st.Page("views/admin.py", title="Admin Dashboard", icon="📊")
+landing_page = st.Page("views/landing.py", title="Landing Page", icon="🏠", default=True)
 
 # Navigasi
-pg = st.navigation([home_page])
+pg = st.navigation([landing_page, home_page, admin_page])
 
 pg.run()
--- views/home.py
+++ views/home.py
@@ -8,9 +8,7 @@ def local_css(file_name):
         with open(file_name) as f:
             st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-local_css("views/home.css")
-
-# st.image("header.png", use_container_width=True)
+local_css("views/css/home.css")
 
 DATA_CLIENT = [
     {
